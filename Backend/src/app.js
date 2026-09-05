7d5e6317aff969494e3d68de320cdd5c25ea2c88
kartikey <kartikey40patel@gmail.com>
2026-09-05 13:33:29 +0530

    download cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const app=express();
 const cookieParser=require('cookie-parser');
+const cors=require('cors');
+
 app.use(cookieParser());
 app.use(express.json())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 /* requires all the routes here*/
 const authRoutes=require('./routes/auth.routes');
 //using all the routes here
