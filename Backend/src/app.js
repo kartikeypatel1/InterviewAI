681974ef498e77eb6c7b0a7bf9f6d2884df5cfd0
kartikey <kartikey40patel@gmail.com>
2026-09-08 18:11:20 +0530

    make one more controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,8 @@ app.use(cors({
 }))
 /* requires all the routes here*/
 const authRoutes=require('./routes/auth.routes');
+const interviewRouter = require('./routes/interview.routes');
 //using all the routes here
 app.use('/api/auth',authRoutes);
+app.use('/api/interview',interviewRouter)
 module.exports=app;
